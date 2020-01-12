081ddc6ccb43eb6a3936ba83163679e1ad7d49ab
Flamestro <19ahmet99@gmai.com>
2020-01-12 23:39:08 +0100

    remove useless files and dirs

--- .idea/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer
-            extends AbstractSecurityWebApplicationInitializer {
-    //no code needed
-}
